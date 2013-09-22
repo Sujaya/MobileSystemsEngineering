6af7ad00751f9e12ef7f74d447c7ea1db5417208
Sujaya A Maiyya <sujayamaiya@gmail.com>
2013-09-22 21:39:36 +0530

    lab5
    lab5 iOs program

---
+++ lab5/loginPRS/loginPRSTests/loginPRSTests.h
@@ -0,0 +1,13 @@
+//
+//  loginPRSTests.h
+//  loginPRSTests
+//
+//  Created by MSE on 19/09/13.
+//  Copyright (c) 2013 MSE. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface loginPRSTests : SenTestCase
+
+@end
---
+++ lab5/loginPRS/loginPRSTests/loginPRSTests.m
@@ -0,0 +1,32 @@
+//
+//  loginPRSTests.m
+//  loginPRSTests
+//
+//  Created by MSE on 19/09/13.
+//  Copyright (c) 2013 MSE. All rights reserved.
+//
+
+#import "loginPRSTests.h"
+
+@implementation loginPRSTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in loginPRSTests");
+}
+
+@end
